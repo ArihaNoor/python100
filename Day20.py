3f3f02407297c04ec93a50e5f689758c3371b02e
arihanoor <arihanoor0@gmail.com>
2025-06-17 19:35:01 +0500

    functions

---
+++ Day20.py
@@ -0,0 +1,20 @@
+#Functions 
+
+
+def isLesser(a,b):
+    if a<b:
+        print("A is Lesser than B")
+    else:
+        print("B is Lesser than A")
+        
+
+isLesser(1,3)
+
+#Pass keyword used to write the function definition in future
+def isGreater(a,b):
+    pass
+
+#There are 2 types of functions 
+    #Built in 
+    #User Defined 
+    
