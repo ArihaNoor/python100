be1f6dc2d2733a5866df54c40d7ef11e3f108d5a
arihanoor <arihanoor0@gmail.com>
2025-06-17 16:34:39 +0500

    String Slicing

---
+++ Day12.py
@@ -0,0 +1,17 @@
+# Strings 
+
+#Slicing in String 
+
+str = "Ariha Noor"
+
+#print the 5 characters (exclusively 5)
+print(str[0:7])
+
+#it actually works like len(fruit)-3
+print(str[0:-3])
+
+#it actually works like len(fruit)-3
+print(str[-3:-1])
+
+
+print(len(str))
