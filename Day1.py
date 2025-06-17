2f7d7d1d4f4c77b56d012b7cb850c1ee6adab161
arihanoor <arihanoor0@gmail.com>
2025-06-17 15:35:18 +0500

    day2

---
+++ Day1.py
@@ -0,0 +1,10 @@
+# Modules and Pip 
+
+# Modules are like borrowing others code, we don't know the implementation but we can use it.
+
+# there are 2 types of modules
+    # Internal Module : Which came built in with python 
+    # External Module : Which we can install ourselves 
+    
+print("hello world")
+
---
+++ Day2.py
@@ -0,0 +1,14 @@
+# Variables are same like containers stored in the memory (RAM)
+
+# There are different data types 
+
+a=1
+print(a)
+
+print(type(a))
+
+
+b = "Ariha"
+print(b)
+
+print(type(b))
