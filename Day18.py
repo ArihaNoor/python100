ac0a362d57f922e337d0770f3d4811d72523ecdf
arihanoor <arihanoor0@gmail.com>
2025-06-17 19:01:40 +0500

    emulating do while loop

---
+++ Day18.py
@@ -0,0 +1,36 @@
+#While Loop 
+
+
+i = 0
+
+while i<20:
+    # print(i)
+    i = i+1
+    
+# We use while loop with complex conditions mostly not with numbers
+
+# i=0
+# while(i<=38):
+    # i = int(input("Enter Number: "))
+    # print(i)
+# print("Done with Loop......")
+
+
+# While with Else 
+
+# count = 5
+# while(count>0):
+#     print(count)
+#     count = count-1
+# else:
+#     print("I am inside else")
+    
+    
+# Emulating Do whole loop in python 
+
+i=0
+while True:
+    print(i)
+    i = i+1
+    if(i%100==0):
+        break
---
+++ Day19.py
@@ -0,0 +1,13 @@
+# break and continue 
+
+#break will stop the iteration
+for i in range(12):
+    if(i==10):
+         break
+    print(i)
+ 
+#continue will skip the iteration   
+for i in range(12):
+    if(i==10):
+         continue
+    print(i)
