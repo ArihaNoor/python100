2f73177b86de8a5197af2ac0ce666d5e3915be5f
arihanoor <arihanoor0@gmail.com>
2025-06-17 16:15:44 +0500

    Strings in python

---
+++ Day10-11.py
@@ -0,0 +1,28 @@
+#Input function 
+
+a = input("Enter your name: ")
+
+print(a)
+
+#Strings 
+
+name = "Ariha"
+
+# we can write strings on multiple lines 
+
+fruit = '''Hey, My Fvrt 
+fruit is mango. 
+What about you?
+'''
+
+print(fruit)
+
+# In python string is likely an array of characters 
+
+print(fruit[0])
+
+
+#Loop through the string (its a blackbox to access the characters one by one)
+
+for character in fruit: 
+    print(character)
