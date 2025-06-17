fdaeebc0ef236bc9b39dd64154944513432f4e8a
arihanoor <arihanoor0@gmail.com>
2025-06-17 16:02:37 +0500

    Day9.py

---
+++ Day9.py
@@ -0,0 +1,24 @@
+#Type Casting in python 
+
+a = "1"
+b = "2"
+
+a = int(a)
+b = int(b)
+
+
+print(a+b)
+
+# There are 2 types of type casting 
+
+    #Explicit : Done via developer 
+    #Implicit : Python do itself 
+    
+c = 1.9 
+d = 8
+
+
+#It will convert the answer to float data type 
+# because if it converts it to integer it will cause some data loss
+print(c+d) 
+
