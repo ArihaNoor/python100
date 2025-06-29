dee330241fa9bab71cf81efc6ca6790a2e87fccc
arihanoor <arihanoor0@gmail.com>
2025-06-29 14:17:44 +0500

    Sets

---
+++ Day31-32.py
@@ -0,0 +1,24 @@
+# Sets 
+
+# Set is a collection of distinct objects or values 
+
+new_set = {1,3,4,5,6,3}
+
+print(new_set)
+
+# it doesn't maintain order of the values  
+
+ariha = {}
+
+print(type(ariha)) #its giving type dictionary 
+
+# because the syntax of writing set and dictionary is same 
+
+# if you want to create an empty set you will write it as 
+
+new = set()
+
+print(type(new))
+
+
+# Methods in set for its manipulation
