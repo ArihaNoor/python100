0c66ab0608fb2ca6bd2bd73bc37892fd9a2cf048
arihanoor <arihanoor0@gmail.com>
2025-06-30 12:44:54 +0500

    Difference

--- Day31-32.py
+++ Day31-32.py
@@ -4,13 +4,13 @@
 
 new_set = {1,3,4,5,6,3}
 
-print(new_set)
+# print(new_set)
 
 # it doesn't maintain order of the values  
 
 ariha = {}
 
-print(type(ariha)) #its giving type dictionary 
+# print(type(ariha)) #its giving type dictionary 
 
 # because the syntax of writing set and dictionary is same 
 
@@ -18,7 +18,52 @@ print(type(ariha)) #its giving type dictionary
 
 new = set()
 
-print(type(new))
+# print(type(new))
 
 
 # Methods in set for its manipulation
+
+#Union 
+
+a = {1,3,5,6,7}
+b = {4,6,8,9}
+
+# it will show both sets values, except duplicated, and will not update the original sets 
+# print(a.union(b))
+
+# it will show both sets values, except duplicated, and will update the original sets 
+
+# a.update(b)
+# print(a,b)
+
+
+#Intersection 
+
+# intersection will give intersection of two sets  
+
+# intersection_update will update your sets 
+
+
+# print(a.intersection(b))
+
+# print(" A: ", a)
+# print(" B: ", b)
+
+# print(a.intersection_update(b))
+
+# print(" A: ", a)
+# print(" B: ", b)
+
+
+
+
+# difference will provide symmetric difference of 2 sets 
+
+print(" A: ", a)
+print(" B: ", b)
+print(a.difference(b))
+print(" A: ", a)
+print(" B: ", b)
+print(a.difference_update(b)) #this will remove the elements which are common in both sets 
+print(" A: ", a)
+print(" B: ", b)
