dc4eef3bbda63e2ab77fb53a8e1b238646e2930e
arihanoor <arihanoor0@gmail.com>
2025-07-01 13:10:54 +0500

    virtual enviornment

---
+++ Day43.py
@@ -0,0 +1,37 @@
+# Virtual Enviornments In Python 
+
+# we can create as many virtual enviorments in our machine 
+
+# on the different virtual enviornments we can use different packages version instead of global one
+
+# ----------command to install venv in ubuntu 
+
+# sudo apt install python3.10-venv
+
+# ----------to create a virtual env
+
+# python3 -m venv myenv
+
+# ---------- to activate 
+
+# source myenv/bin/activate
+
+# ---------- to deactivate 
+
+# deactivate 
+
+# --------- to exit from the python shell 
+
+# exit()
+
+# -------- to get all installed packages in the enviornment 
+
+# pip freeze 
+
+# ------- to get all packages in the file 
+
+# pip freeze > requirements.txt
+
+# --- to install all the packages in the requirements file 
+
+# pip install -r requirements.txt
